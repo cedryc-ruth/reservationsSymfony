c0c9498b371de02d971c782a4e6c813b76c9eea6
Cédryc Ruth <ceruth@domepfc.net>
2019-04-27 11:26:02 +0200

    Type, Role, Locality controllers +templates created

---
+++ src/Controller/LocalityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Locality;
+
+class LocalityController extends AbstractController
+{
+    /**
+     * @Route("/locality", name="locality")
+     */
+    public function index()
+    {
+        $repo = $this->getDoctrine()->getRepository(Locality::class);
+        
+        $localities = $repo->findAll();
+        
+        return $this->render('locality/index.html.twig', [
+            'localities' => $localities,
+            'resource' => 'localités',
+        ]);
+    }
+}
---
+++ src/Controller/RoleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Role;
+
+class RoleController extends AbstractController
+{
+    /**
+     * @Route("/role", name="role")
+     */
+    public function index()
+    {
+        $repo = $this->getDoctrine()->getRepository(Role::class);
+        
+        $roles = $repo->findAll();
+        
+        return $this->render('role/index.html.twig', [
+            'roles' => $roles,
+            'resource' => 'rôles',
+        ]);
+    }
+}
