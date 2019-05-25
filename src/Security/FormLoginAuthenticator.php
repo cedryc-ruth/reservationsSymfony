ce9703500a5c17760d331f1aa466d5e18dd62b2a
Cédryc Ruth <ceruth@domepfc.net>
2019-05-25 14:26:29 +0200

    Gestion de la page de profil

--- src/Security/FormLoginAuthenticator.php
+++ src/Security/FormLoginAuthenticator.php
@@ -87,7 +87,7 @@ class FormLoginAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('artist'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
